5b382b63afe79f6c54359dbd5ef910875986fe76
maosor <maosor@hotmail.com>
2018-06-11 16:00:24 -0600

    Alineado de botones de facebook y whatsapp

--- admin/extend/header-online.php
+++ admin/extend/header-online.php
@@ -17,10 +17,10 @@
         <ul class="nav-bar">
            <li><span class="hide-on-small-only">Llámenos al (506) 2101-0042 | WhatsApp (506) 7200-2542</span>
            <li><span class="hide-on-med-and-up">T: (506) 2101-0042 | C: (506) 7200-2542</span>
-           <li class="right"><a href="https://www.facebook.com/BestWayTravelcr" class="right"><img class="orange darken-4 bordeimg circle z-depth-1" src="admin/img/facebook.png" style="padding: 4px;margin: 3px;"> </a>
-           <li class="right"><a href="https://api.whatsapp.com/send?phone=50672002542" class="right"><img class="orange darken-4 bordeimg circle z-depth-1" src="admin/img/whatsapp.png" style="padding: 4px;margin: 3px;"> </a>
+           <li class="right"><a href="https://www.facebook.com/BestWayTravelcr" class="img_top_menu"><img class="orange darken-4 bordeimg circle z-depth-1" src="admin/img/facebook.png" style="padding: 4px;margin: 3px;"> </a>
+           <li class="right" style="width: 40px;"><a href="https://api.whatsapp.com/send?phone=50672002542" class="img_top_menu"><img class="orange darken-4 bordeimg circle z-depth-1" src="admin/img/whatsapp.png" style="padding: 4px;margin: 3px;"> </a>
           <li class="right">
-            <div class="" style="position:static;">
+            <div class="hide-on-med-and-down" style="position:static;">
               <form class="buscar" role="search" method="post" action="paquetes.php">
                 <div class="field" id="searchform">
                   <input id="criterio" value="" name="criterio" placeholder="Escribe ..." type="text">
@@ -46,12 +46,29 @@
       </ul>
 
       <ul class="side-nav" id="mobile-demo">
+        <li class="center orange darken-4">
+          <div class="show-on-med-and-down">
+            <form class="buscar" role="search" method="post" action="paquetes.php">
+              <div class="field" id="searchform">
+                <input id="criterio" value="" name="criterio" placeholder="Escribe ..." type="text">
+                <button type="submit" id="search">BUSCAR</button>
+              </div>
+            </form>
+          </div>
+        </li>
+        <li><div class="divider"></div></li>
         <li><a href="index.php" class="blue-grey-text text-darken-2 center">Nosotros</a></li>
+        <li><div class="divider"></div></li>
         <li><a href="paquetes.php?inter=1" class="blue-grey-text text-darken-2 center">Internacionales</a></li>
+        <li><div class="divider"></div></li>
         <li><a href="paquetes.php?inter=0" class="blue-grey-text text-darken-2 center">Nacionales</a></li>
+        <li><div class="divider"></div></li>
         <li><a href="paquetes.php?oferta=1" class="blue-grey-text text-darken-2 center">Ofertas</a></li>
+        <li><div class="divider"></div></li>
         <li><a href="contacto.php" class="blue-grey-text text-darken-2 center">Contactenos</a></li>
+        <li><div class="divider"></div></li>
         <li><a href="cotizar.php" class="orange-text text-darken-4 center enf">Cotizar</a></li>
+        <li><div class="divider"></div></li>
       </ul>
       </div>
     </nav>
